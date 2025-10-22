9e8cf99465d6dce80288ecdb1bbfa7a29a1fc7e9
vendor <localfalafelvendor@gmail.com>
2025-10-22 10:04:01 +0530

    mac random

--- backup.sh
+++ backup.sh
@@ -9,10 +9,12 @@ mkdir 	"$target_dir/dotfiles/" \
 	"$target_dir/dotfiles/neofetch" \
 	"$target_dir/dotfiles/kitty" \
 	"$target_dir/dotfiles/fontconfig" \
-	"$target_dir/dotfiles/wofi"
+	"$target_dir/dotfiles/wofi" \
+	"$target_dir/dotfiles/NetworkManager"
 cp -r $HOME/.config/hypr/* $target_dir/dotfiles/hypr/ && echo "Copied Hypr!"
 cp -r $HOME/.config/waybar/* $target_dir/dotfiles/waybar/ && echo "Copied Waybar!"
 cp -r $HOME/.config/neofetch/* $target_dir/dotfiles/neofetch/ && echo "Copied Neofetch!"
 cp -r $HOME/.config/kitty/* $target_dir/dotfiles/kitty/ && echo "Copied Kitty!"
 cp -r $HOME/.config/fontconfig/* $target_dir/dotfiles/fontconfig/ && echo "Copied FontConfig!"
 cp -r $HOME/.config/wofi/* $target_dir/dotfiles/wofi/ && echo "Copied Wofi!"
+cp -r /etc/NetworkManager/conf.d/00-macrandomize.conf $target_dir/dotfiles/NetworkManager/ && echo "Copied Network Manager Randomize Mac conf"
--- dotfinstall.sh
+++ dotfinstall.sh
@@ -4,6 +4,7 @@ cp -r kitty "$HOME/.config/" && echo "kitty copied into $HOME/.config/"
 cp -r neofetch "$HOME/.config/" && echo "neofetch copied into $HOME/.config/"
 cp -r waybar "$HOME/.config/" && echo "waybar copied into $HOME/.config/"
 cp -r wofi "$HOME/.config/" && echo "wofi copied into $HOME/.config/"
+cp -r NetworkManager/* "/etc/NetworkManager/conf.d/"&& echo "copied networkmanager random mac" 
 
 
 
